52613eca608fb4a8ec8264c508b463d5730a9c38
Alfred <alfredn@kth.se>
2024-02-23 11:37:00 +0100

    c

--- _OUR_SCRIPTS/install_casanovo_env.sh
+++ _OUR_SCRIPTS/install_casanovo_env.sh
@@ -5,10 +5,10 @@ conda activate casanovo_2.1.1
 # conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
 
 # Install casanovo (locally from our fork)
-pip install .
+python -m pip install .
 
 # OR install the specific release "releases/download/v2.1.1/casanovo_excl_yeast.ckpt"
-pip install casanovo==2.1.1
+python -m pip install casanovo==2.1.1
 
 # we need pyarrow for the conversion script
 pip install pyarrow
